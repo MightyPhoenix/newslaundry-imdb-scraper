3610949eb61829adf09912f40b3228d2b0152651
MightyPhoenix <agnibesh00mukherjee@gmail.com>
2020-08-25 00:21:31 +0530

    Task Completed

--- app.js
+++ app.js
@@ -1,34 +1,71 @@
 const puppet = require("puppeteer");
+const csv = require("csv-parser");
+const fs = require("fs");
+const ObjectsToCsv = require("objects-to-csv");
+const results = [];
+let finalData = [];
 
-const scraper = async () => {
+const scraper = async ({ tconst, title, year }) => {
+  // Launch Puppet Browser
   const browser = await puppet.launch({
-    headless: false,
+    headless: true,
     defaultViewport: null,
   });
+  // Open New Tab
   const tab1 = await browser.newPage();
-  await tab1.goto("https://www.imdb.com/title/tt7059844/fullcredits");
+  console.log(`Opening ${tconst}-->${title}...`);
+  // Open Link
+  await tab1.goto(`https://www.imdb.com/title/${tconst}/fullcredits`);
   await tab1.waitFor(".cast_list");
-
+  // Start Evaluationg (Scraping)
+  console.log("Scraping Started!");
   let nameData = await tab1.evaluate(() => {
-    const names = [];
+    const castList = [];
     let rows = document.querySelectorAll("table[class='cast_list']>tbody>tr");
-
     rows.forEach((ele) => {
-      let nameJson = {};
+      let cast = {};
       try {
-        nameJson.name = ele.querySelector("td:nth-child(2)").innerText;
-        nameJson.character = ele.querySelector("td:nth-child(4)").innerText;
+        cast.name = ele.querySelector("td:nth-child(2)").innerText;
+        cast.character = ele.querySelector("td:nth-child(4)").innerText;
       } catch (exception) {}
-      if (nameJson.name) {
-        names.push(nameJson);
+      if (cast.name) {
+        castList.push(cast);
       }
     });
-    return names;
+    return castList;
   });
-
-  console.table(nameData);
-  console.log(nameData.length);
+  // Data Formatting {tconst,title,year,actor,character}
+  let cleanUp = nameData.map((i) => {
+    return { tconst: tconst, title: title, year: year, ...i };
+  });
+  //Close Browser
   browser.close();
+  console.log(`Scraping Done!`);
+  return cleanUp;
 };
 
-scraper();
+fs.createReadStream("input.csv")
+  .pipe(csv())
+  .on("data", (data) => results.push(data))
+  .on("end", async () => {
+    for (let i = 0; i < results.length; i++) {
+      console.log(`${i + 1}/${results.length}`);
+      charData = await scraper({
+        tconst: results[i].tconst,
+        title: results[i].title,
+        year: results[i].year,
+      });
+      try {
+        finalData = await [...finalData, ...charData];
+        // console.table(finalData);
+      } catch (exception) {
+        console.log(exception);
+      }
+    }
+    // console.table(finalData);
+    (async () => {
+      const csv = new ObjectsToCsv(finalData);
+      // Save to file:
+      await csv.toDisk("./output.csv");
+    })();
+  });
